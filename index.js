edfe11b31bdd764f121e06a8dd8eb899d5aaaf81
icnpdesign <inp@bu.edu>
2017-11-09 15:13:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ function printBadges (names) {
 printBadges(names);
 
 function maybeTrue() {
-  return Math.random() >= 0.5;
+  return Math.random() <= 0.5;
 }
 
 function tailsNeverFails () {
-  let tails = 0;
+  let tails = 1;
   while (maybeTrue()) {
    tails++;
   }
